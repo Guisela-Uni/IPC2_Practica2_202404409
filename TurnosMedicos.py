66a78fac5f238244de9bd1bbd96fed346edd50b4
Guisela-ai19 <guisellamonroy123@gmail.com>
2025-09-09 21:50:45 -0600

    infonodo y nodo

---
+++ TurnosMedicos.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+class InfoNodo(): 
+    def desplegar(self):
+        pass 
+    def EsIgualALLave(self,placa): 
+        pass
+
+class Nodo: 
+    def __init__(self,info):
+        self.info = info 
+        self.siguiente = None 
